36e3325ebc626bb2c8310f155113943dd3ac9988
ben <futureappworker@gmail.com>
2022-10-13 07:37:08 +0800

    Add basic 404 500 handler

--- server.js
+++ server.js
@@ -21,6 +21,16 @@ app.use(serveStatic('public', { index: ['index.html'] }));
 
 app.use('/auth', authRouter);
 
+app.get('*', (req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// eslint-disable-next-line
+app.use((err, req, res, next) => {
+  res.status(500).send('Internal Server Error');
+});
+
 app.listen(port, () => {
-  // console.log(`Example app listening on port ${port}`);
+  // eslint-disable-next-line
+  console.log(`Example app listening on port ${port}`);
 });
